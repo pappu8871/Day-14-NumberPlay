44d62142d46714218dfdae862857e5724389cc96
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 12:11:32 +0530

    Added:NumberPlayUC1

--- NumberPlay.java
+++ NumberPlay.java
@@ -1,9 +1,21 @@
 package com.classpractice.numberplay;
 
-public class WelcomeNum {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
-	public static void main(String[] args) {
-	System.out.println("Welcome NumberPlay");
-	}
+public class NumberPlayUC1 {
+	  public static void main(String[] args) {
+		//  Use collection
+			List<Integer>myNumberList = new ArrayList<Integer>();
+			    for(int i = 0; i<5; i++) myNumberList.add(i);
+			
+			//Implicit Lambda Function to check even
+			    
+			    myNumberList.stream().forEach(n -> {
+			    	System.out.println("Stream for each value " +n);
+			    });
+			    
+			}
 
 }
