fb49d0a487342918d05f52e0a98a17aa17aae7ed
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 12:36:25 +0530

    Added:NnmberPlayUC2, Takinf double

--- NumberPlay.java
+++ NumberPlay.java
@@ -2,20 +2,21 @@ package com.classpractice.numberplay;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
-public class NumberPlayUC1 {
-	  public static void main(String[] args) {
+public class NumberPlayUC2 {
+
+	     public static void main(String[] args) {
 		//  Use collection
 			List<Integer>myNumberList = new ArrayList<Integer>();
 			    for(int i = 0; i<5; i++) myNumberList.add(i);
 			
-			//Implicit Lambda Function to check even
-			    
-			    myNumberList.stream().forEach(n -> {
-			    	System.out.println("Stream for each value " +n);
-			    });
-			    
-			}
-
+			    //Apply operation on the Stream
+			    Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+			    List<Double>streamList = (List<Double>) myNumberList.stream()
+			    		                 .map(toDoubleFunction)
+			    		                 .collect(Collectors.toList());
+			    	System.out.println("Stream Print Double List " + streamList);
+			   }
 }
