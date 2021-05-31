dda7cdaa94e9b353286507ae0aebc823f1e726df
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 12:41:58 +0530

    Added:Taking even num from double

--- NumberPlay.java
+++ NumberPlay.java
@@ -3,20 +3,23 @@ package com.classpractice.numberplay;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class NumberPlayUC2 {
-
-	     public static void main(String[] args) {
-		//  Use collection
-			List<Integer>myNumberList = new ArrayList<Integer>();
-			    for(int i = 0; i<5; i++) myNumberList.add(i);
-			
-			    //Apply operation on the Stream
-			    Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
-			    List<Double>streamList = (List<Double>) myNumberList.stream()
-			    		                 .map(toDoubleFunction)
-			    		                 .collect(Collectors.toList());
-			    	System.out.println("Stream Print Double List " + streamList);
-			   }
+public class NumberPlayUC3 {
+	 public static void main(String[] args) {
+			//  Use collection
+				List<Integer>myNumberList = new ArrayList<Integer>();
+				    for(int i = 0; i<5; i++) myNumberList.add(i);
+				
+				    //Apply operation on the Stream
+				    Predicate<Integer> isEvenFunction = n -> n > 0 && n%2 == 0 ;
+				    Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
+				    List<Double>streamList = (List<Double>) myNumberList.stream()
+				    		                 .filter(isEvenFunction)
+				    		                 .map(toDoubleFunction)
+				    		                 .collect(Collectors.toList());
+				    	System.out.println("Stream Print Double List " + streamList);
+	       //Store the Transformed double value in to a new list			  
+	 }
 }
