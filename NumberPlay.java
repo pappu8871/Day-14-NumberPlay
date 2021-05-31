3ddc035fce248a7586ea5ff8f4b93e49074d23ea
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 12:58:07 +0530

    Added:Geeting Average

--- NumberPlay.java
+++ NumberPlay.java
@@ -6,39 +6,19 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class NumberPlayUC6 {
-  
-	public static void main(String[] args){
+public class NumberPlayUC7 {
+
+   public static void main(String[] args){
 	
-	// Use collection
-				List<Integer>myNumberList = new ArrayList<Integer>();
-				    for(int i = 0; i<5; i++) myNumberList.add(i);
+	       // Use collection
+   List<Integer>myNumberList = new ArrayList<Integer>();
+   for(int i = 0; i<5; i++) myNumberList.add(i);
 				
-				    //Apply operation on the Stream
-				    //peek first Even Numbere
-				    Predicate<Integer> isEvenFunction = n -> n > 0 && n%2 == 0 ;
-				    Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
-				    Integer first =  myNumberList.stream()
-				    		                .filter(isEvenFunction)
-				    		                .peek(n -> System.out.println("peak Even Number" +n))
-				    		                .findFirst()
-				    		                .orElse(null);
-         
-				    		System.out.println("peek first Even Value " + first);
-	                 //Minimum Even number
-				    		Integer min =  myNumberList.stream()
-				    					.filter(isEvenFunction)
-				    					.min((n1,n2) -> n1-n2)
-				    					.orElse(null);
- 
-		    		         System.out.println("peek min Value " + min);		
-                 // Maximum Even Number
-		    		  Integer max =  myNumberList.stream()
-		    				  		.filter(isEvenFunction)
-    		                     	.max(Comparator.comparing(Integer::intValue))
-    		                     	.orElse(null);
-
-    		             System.out.println("peek max Value " + max);		    		
-
-}
+ //Apply operation on the Stream
+//Average Value
+  Integer sum = myNumberList.stream()
+				 .reduce(0, Integer::sum);
+				  long count = myNumberList.stream().count();
+System.out.println("Average of "+sum+"/"+count+" = "+sum/count);
+     }
 }
