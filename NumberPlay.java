75f213f1f276194febc98ea7413e09e28daca0ce
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-31 12:51:19 +0530

    Added:NumberPlayUC5, first Even num

--- NumberPlay.java
+++ NumberPlay.java
@@ -6,20 +6,22 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class NumberPlayUC4 {
-
+public class NumberPlayUC5 {
 	 public static void main(String[] args) {
 			//  Use collection
 				List<Integer>myNumberList = new ArrayList<Integer>();
 				    for(int i = 0; i<5; i++) myNumberList.add(i);
 				
 				    //Apply operation on the Stream
+				    //peek first Even Numbere
 				    Predicate<Integer> isEvenFunction = n -> n > 0 && n%2 == 0 ;
 				    Function<Integer,Double> toDoubleFunction = Integer::doubleValue;
-				    List<Double>streamList = (List<Double>) myNumberList.stream()
-				    		                 .filter(isEvenFunction)
-				    		                 .map(toDoubleFunction)
-				    		                 .collect(Collectors.toList());
-				    	System.out.println("Stream Print Double List " + streamList);
+				    Integer first =  myNumberList.stream()
+				    		                .filter(isEvenFunction)
+				    		                .peek(n -> System.out.println("peak Even Number" +n))
+				    		                .findFirst()
+				    		                .orElse(null);
+         
+				    		System.out.println("peek first Even Value " + first);
 	        }
-       }
+}
